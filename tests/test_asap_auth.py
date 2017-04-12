bb9d5a7236a013c0d471595481f4a42599291b65
Jason Friedland <jason@friedland.id.au>
2017-04-12 21:38:14 +1000

    Minor test update

--- tests/test_asap_auth.py
+++ tests/test_asap_auth.py
@@ -7,3 +7,4 @@ def test_parse_config(asap_config_file):
     assert asap_config.iss == 'webapp/admin'
     assert asap_config.kid == 'webapp/admin/dev.pem'
     assert 'asapservice' in asap_config.aud
+    assert asap_config.private_key == 'PEM-encoded-key-goes-here'
